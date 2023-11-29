c8e63d7471afd6a642e8e026de94add4359a1ebe
harpolino5 <harpolino5@gmail.com>
2023-11-29 22:23:38 +0300

    Co-authored-by: Senichak Bohdan <BSenichak@users.noreply.github.com> Co-authored-by: Viera14 <Viera14@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ document.querySelector("header").addEventListener("click", ()=>{
   new Audio("assets/audio/owl-hooting-48028.mp3").play()
 })
 
+gsap.registerPlugin(ScrollTrigger) 
+gsap.to(".item", {
+    scrollTrigger: {
+        trigger: ".item",
+        start: "top 80%",
+        end: "bottom",
+        markers: true,
+        scrub: true,
+        toggleActions: "restart pause reverse pause"
+    },
+    x: "50vw",
+    backgroundColor: "red",
+    duration: 3,
+    // rotation: 180,
+    // ease: "elastic.inOut(1,0.4)"
+  });
+
 
 
 
